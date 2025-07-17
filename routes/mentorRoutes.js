0f75b17f8b556ba8c4a81e497a9d7e8fd64ca268
Ufuomsky95 <orifeufuoma@gmail.com>
2025-07-17 13:01:09 +0100

    latest1 commit 17th

--- routes/mentorRoutes.js
+++ routes/mentorRoutes.js
@@ -4,6 +4,6 @@ import { addMentor, getMentor } from "../controller/mentorController.js"
 const mentorRoutes = express.Router()
 
 mentorRoutes.post("/addMentor", addMentor )
-mentorRoutes.get("/get-mentors", getMentor )
+mentorRoutes.get("/get-mentors", getMentor ) 
 
 export default mentorRoutes
